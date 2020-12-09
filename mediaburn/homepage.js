2947527c318ec13978db1cd67cec0005ef4542c0
deannadarling <43623223+deannadarling@users.noreply.github.com>
2020-12-09 17:18:20 -0500

    addingmediaburnfinal

---
+++ mediaburn/homepage.js
@@ -0,0 +1,16 @@
+var i = 0;
+var txt = 'Welcome to our site ... :)';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("draw").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }  
+}
+function myFunction() {
+    
+  document.getElementById("dosomething").style.color = "#2813d1";
+    
+}
