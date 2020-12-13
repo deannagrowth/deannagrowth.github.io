3765b812fd778322c77deb77c93b295e3ab472e8
deannadarling <43623223+deannadarling@users.noreply.github.com>
2020-12-12 20:10:22 -0500

    hehe

--- mediaburn/homepage.js
+++ mediaburn/homepage.js
@@ -19,3 +19,22 @@ function myFunction() {
   document.getElementById("dosomething").style.color = "#FF0000";
     
 }
+
+function dragStart(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+}
+
+function dragging(event) {
+  document.getElementById("demo").innerHTML = "The p element is being dragged";
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+  document.getElementById("demo").innerHTML = "The p element was dropped";
+}
